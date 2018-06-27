0af0f58dc88d420e8b5066ead639606908277f8a
Akshata Mohanty <akshatamohanty@gmail.com>
2018-06-27 22:35:25 +0800

    Added npm-run-all package to run client and server simultaneously

--- routes/generate.js
+++ routes/generate.js
@@ -2,7 +2,7 @@
 * @Author: Akshata
 * @Date:   2018-06-27 21:55:39
 * @Last Modified by:   Akshata
-* @Last Modified time: 2018-06-27 21:56:29
+* @Last Modified time: 2018-06-27 22:31:41
 */
 
 
@@ -11,7 +11,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Test API');
+  res.send('Test API::Test API::Test API');
 });
 
 module.exports = router;

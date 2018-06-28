b1d9c788362a700c71ef2d482ec2a05461d0dfb2
Akshata Mohanty <akshatamohanty@gmail.com>
2018-06-29 02:18:58 +0800

    Added allowed origins

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 * @Author: Akshata
 * @Date:   2018-06-27 21:55:39
 * @Last Modified by:   Akshata
-* @Last Modified time: 2018-06-28 19:30:23
+* @Last Modified time: 2018-06-29 02:33:14
 */
 
 
@@ -20,7 +20,12 @@ var app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    var allowedOrigins = ['http://localhost:4200', 'https://akshatamohanty.github.io'];
+    var origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');

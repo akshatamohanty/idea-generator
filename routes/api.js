9131b38c6d9718fb38e5d87ca73ea809ae68e6b4
Akshata Mohanty <akshatamohanty@gmail.com>
2018-06-29 20:30:16 +0800

    Changed API definition

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@
 * @Author: Akshata
 * @Date:   2018-06-27 21:55:39
 * @Last Modified by:   Akshata
-* @Last Modified time: 2018-06-28 19:37:39
+* @Last Modified time: 2018-06-29 20:29:24
 */
 
 
@@ -14,7 +14,10 @@ router.get('/generate', function(req, res, next) {
 	let wordA = req.query.wordA;
 	let wordB = req.query.wordB; 
 
-	let results = Array.from({length: 1000}, () => `${Math.floor(Math.random()*10)} :: ${wordA} :: ${wordB}`);
+	let lorem_ipsum_text = "Lorem ipsum ut aute cupidatat eu enim ut amet dolor dolore est velit commodo magna culpa ullamco elit dolor labore."
+	let random_int = Math.floor(Math.random()*lorem_ipsum_text.length);
+
+	let results = Array.from({length: 1000}, () => `${ lorem_ipsum_text.slice(random_int, random_int + Math.floor(Math.random()*lorem_ipsum_text.length) ) }`);
     res.send(JSON.stringify(results));
 });
 
